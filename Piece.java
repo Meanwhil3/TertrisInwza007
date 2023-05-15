be4e3a2546590c0fe8f0870245686716e450323c
Kankorn Neo <86392826+seernioh@users.noreply.github.com>
2023-05-15 22:26:36 +0700

    Add a basic Tetraminos funtion
    Make a foundation for the game to be somewhat functional.

--- Piece.java
+++ Piece.java
@@ -1,67 +1,89 @@
+package TetrisPJ;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Piece {
-    private final Point[][] pieces = {{mp(0, 4), mp(0, 5), mp(1, 4), mp(1, 5)},
-								 {mp(1, 3), mp(1, 4), mp(1, 5), mp(1, 6)},
-								 {mp(0, 3), mp(1, 3), mp(1, 4), mp(1, 5)},
-								 {mp(0, 5), mp(1, 3), mp(1, 4), mp(1, 5)},
-								 {mp(1, 3), mp(1, 4), mp(0, 4), mp(0, 5)},
-								 {mp(0, 3), mp(0, 4), mp(1, 4), mp(1, 5)},
-								 {mp(1, 3), mp(1, 4), mp(1, 5), mp(0, 4)}};
-	// auxiliary method to create a new Point object
-	private Point mp (int x, int y) {
-		return new Point(x, y);
-	}
-	@Deprecated
+    // starting position for each pieces and create a new Point object 
+    private static final Point[][] pieces = {
+        {new Point(0, 4), new Point(0, 5), new Point(1, 4), new Point(1, 5)},
+        {new Point(1, 3), new Point(1, 4), new Point(1, 5), new Point(1, 6)},
+        {new Point(0, 3), new Point(1, 3), new Point(1, 4), new Point(1, 5)},
+        {new Point(0, 5), new Point(1, 3), new Point(1, 4), new Point(1, 5)},
+        {new Point(1, 3), new Point(1, 4), new Point(0, 4), new Point(0, 5)},
+        {new Point(0, 3), new Point(0, 4), new Point(1, 4), new Point(1, 5)},
+        {new Point(1, 3), new Point(1, 4), new Point(1, 5), new Point(0, 4)}
+};
+
+    // represents a point on the grid
+    static class Point {
+        int r, c;
+        Point (int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    // generates a random piece
     public Active getActive () {
-		int id = (int)(Math.random()*7);
-		Point[] newPiece = new Point[4];
-		for (int i = 0; i < 4; i++)
-			newPiece[i] = new Point(pieces[id][i].r, pieces[id][i].c);
-		return new Active(newPiece, id+1);
-	}
+        int id = (int)(Math.random()*7);
+        Point[] newPiece = {
+            new Point(pieces[id][0].r, pieces[id][0].c), 
+            new Point(pieces[id][1].r, pieces[id][1].c), 
+            new Point(pieces[id][2].r, pieces[id][2].c), 
+            new Point(pieces[id][3].r, pieces[id][3].c)
+        };
+        return new Active(newPiece, id+1);
+    }
+
+    // return a piece with a specific id
     public Active getActive (int id) {
-		Point[] newPiece = new Point[4];
-		for (int i = 0; i < 4; i++)
-			newPiece[i] = new Point(pieces[id][i].r, pieces[id][i].c);
-		return new Active(newPiece, id+1);
-	}
-    public int[] getPermutation () {
+        Point[] newPiece = {
+            new Point(pieces[id][0].r, pieces[id][0].c), 
+            new Point(pieces[id][1].r, pieces[id][1].c), 
+            new Point(pieces[id][2].r, pieces[id][2].c), 
+            new Point(pieces[id][3].r, pieces[id][3].c)
+        };
+        return new Active(newPiece, id+1);
+    }
+
+    //represents the active piece
+    static class Active {
+        Point[] pos;
+        int id;
+        int lor, hir, loc, hic;
+        int state = 0;
+        Active (Point[] pos, int id) {
+            this.pos = pos;
+            this.id = id;
+            if (id != 2) {
+                lor = 0; hir = 2;
+                loc = 3; hic = 5;
+            }
+            else {
+                lor = 0; hir = 3;
+                loc = 3; hic = 6;
+
+            }
+        }
+    }
+
+    // function to permute the pieces
+    private void permute(int i, int[] a) {
+        if (i == 6)
+            return;
+        int swap = ThreadLocalRandom.current().nextInt(i, 7);
+        int temp = a[i];
+        a[i] = a[swap];
+        a[swap] = temp;
+        permute(i + 1, a);
+    }
+
+    // generates a permutation of the seven pieces and returns it
+	public int[] getPermutation () {
 		int[] res = new int[7];
 		for (int i = 0; i < 7; i++)
 			res[i] = i;
 		permute(0, res);
 		return res;
-	}
-    private void permute (int i, int[] a) {
-		if (i == 6)
-			return;
-		int swap = (int)(Math.random()*(6-i) + i + 1);
-		int temp = a[i];
-		a[i] = a[swap];
-		a[swap] = temp;
-		permute(i+1, a);
-	}
-	static class Active {
-		Point[] pos;
-		int id;
-		int lor, hir, loc, hic;
-		int state = 0;
-		Active (Point[] pos, int id) {
-			this.pos = pos;
-			this.id = id;
-			if (id != 2) {
-				lor = 0; hir = 2;
-				loc = 3; hic = 5;
-			} else {
-				lor = 0; hir = 3;
-				loc = 3; hic = 6;
-			}
-		}
-	}
-	static class Point {
-		int r, c;
-		Point (int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
 	}
 }
