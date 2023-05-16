0766893dbbdce39de102f885303e26f5aa18ed0b
Mean <anuwattipong123@gmail.com>
2023-05-17 01:25:42 +0700

    Finish Piece

--- TetrisPJ/Piece.java
+++ TetrisPJ/Piece.java
@@ -1,6 +1,12 @@
 package TetrisPJ;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.Collections;
 
 public class Piece {
     // starting position for each pieces and create a new Point object 
@@ -22,10 +28,36 @@ public class Piece {
             this.c = c;
         }
     }
+    
+    public static ArrayList<Integer> getRandomNumbers(int count, int min, int max) {
+        List<Integer> availableNumbers = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            availableNumbers.add(i);
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        Random random = new Random();
+
+        while (uniqueNumbers.size() < count) {
+            int randomNumber = random.nextInt(availableNumbers.size());
+            int number = availableNumbers.get(randomNumber);
+            uniqueNumbers.add(number);
+            availableNumbers.remove(randomNumber);
+        }
+
+        ArrayList<Integer> randomList = new ArrayList<>(uniqueNumbers);
+        Collections.shuffle(randomList); // เรียงลำดับแบบสุ่ม
+        return randomList;
+    }
+
+
 
     // generates a random piece
     public Active getActive () {
-        int id = (int)(Math.random()*7);
+        ArrayList<Integer> randomList = getRandomNumbers(7, 0, 6);
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(randomList.size()); // เลือกเลขสุ่มออกมาในช่วงตั้งแต่ 0 ถึง size-1
+        int id = randomList.get(randomIndex);
         Point[] newPiece = {
             new Point(pieces[id][0].r, pieces[id][0].c), 
             new Point(pieces[id][1].r, pieces[id][1].c), 
@@ -67,23 +99,23 @@ public class Piece {
         }
     }
 
-    // function to preview the pieces
-    private void preview(int i, int[] a) {
+    // function to permute the pieces
+    private void permute(int i, int[] a) {
         if (i == 6)
             return;
         int swap = ThreadLocalRandom.current().nextInt(i, 7);
         int temp = a[i];
         a[i] = a[swap];
         a[swap] = temp;
-        preview(i + 1, a);
+        permute(i + 1, a);
     }
 
     // generates a permutation of the seven pieces and returns it
-	public int[] getPreview () {
+	public int[] getPermutation () {
 		int[] res = new int[7];
 		for (int i = 0; i < 7; i++)
 			res[i] = i;
-		preview(0, res);
+		permute(0, res);
 		return res;
 	}
 }
