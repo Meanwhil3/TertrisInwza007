a9cebccb9223e7cb5958d7f258c5ed2efd725b7a
nopbys <118421368+nopbys@users.noreply.github.com>
2023-05-19 01:18:23 +0700

    Update Popo.java

--- Popo.java
+++ Popo.java
@@ -4,60 +4,72 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Popo implements ActionListener {
-    private int numPlayers;
     private JFrame frame;
-    private JPanel panel,p2;
-    private JButton button;
+    private JPanel p1,p2,p3,p4;
+    private JButton b1,b2;
     private JTextField textField;
-    private JLabel label;
+    private JLabel lt,lh;
     private Window window;
 
     Popo() {
         frame = new JFrame();
-        button = new JButton("ENTER");
-        textField = new JTextField(10);
-        panel = new JPanel();
+        p1 = new JPanel();
         p2 = new JPanel();
-        label = new JLabel("HOW MANY PLAYERS");
-
-        panel.setLayout(new FlowLayout());
-        frame.setLayout(new GridLayout(2, 1));
-        panel.add(label);
-        panel.add(textField);
-        frame.add(panel);
+        p3 = new JPanel();
+        lt = new JLabel("TETRIS");
+        lh = new JLabel("HOW MANY PLAYER");
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        p4 = new JPanel();
+        p1.add(lt);
+        p2.add(lh);
+        p3.add(b1);p3.add(b2);
+        // toktang
+        p1.setBackground(Color.BLACK);
+        p2.setBackground(Color.BLACK);
+        p3.setBackground(Color.BLACK);
+        p4.setBackground(Color.BLACK);
+        lt.setForeground(Color.GREEN);
+        lh.setForeground(Color.RED);
+        lh.setFont(new Font("Verdana", Font.BOLD, 24));
+        lt.setFont(new Font("Verdana", Font.BOLD, 35));
+        p1.setLayout(new FlowLayout());
+        p2.setLayout(new FlowLayout());
+        p3.setLayout(new FlowLayout());
+        b1.setPreferredSize(new Dimension(75, 50));
+        b2.setPreferredSize(new Dimension(75, 50));
+        b1.setFocusable(false);
+        b2.setFocusable(false);
+        b1.setFont(new Font("Verdana", Font.BOLD, 15));
+        b2.setFont(new Font("Verdana", Font.BOLD, 15));
+        b1.setForeground(Color.WHITE);
+        b2.setForeground(Color.WHITE);
+        b1.setBackground(Color.BLUE);
+        b2.setBackground(Color.BLUE);
+        frame.setLayout(new GridLayout(4,1));
+        frame.add(p1);
         frame.add(p2);
-        p2.add(button);
-
-        button.setBackground(Color.WHITE);
-        button.setForeground(Color.RED);
-        button.setFocusPainted(false);
-
+        frame.add(p3);
+        frame.add(p4);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        
+        
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-
-        button.addActionListener(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource().equals(button)) {
-            if (textField.getText().equals("")) {
-                JOptionPane.showMessageDialog(null, "ENTER NUMBER");
-            } else {
-                setNumPlayers(Integer.parseInt(textField.getText()));
-                textField.setText("");
-                window = new Window(numPlayers);
+        if (e.getSource().equals(b1)) {
+            window = new Window(1);
                 frame.dispose();
-            }
         }
-    }
-
-    public int getNumPlayers() {
-        return numPlayers;
-    }
-
-    public void setNumPlayers(int numPlayers) {
-        this.numPlayers = numPlayers;
+        if (e.getSource().equals(b2)) {
+            window = new Window(2);
+                frame.dispose();
+        }
     }
 }
