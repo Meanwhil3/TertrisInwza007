25ccb6fba038ad874a976caaca3f7f1d2732d5a7
nopbys <118421368+nopbys@users.noreply.github.com>
2023-05-19 02:06:25 +0700

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+public class main {
+    public static void main(String[] args)throws IOException {
+        new Popo();
+    }
+}
